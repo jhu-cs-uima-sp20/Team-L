eba4eaf08c0016530563a74a35a0acb15bb19f23
Joshua Kim <jkim567@jhu.edu>
2020-04-05 17:29:44 -0500

    implemented spinner and changed create listings into activity

--- app/src/main/java/com/example/jhump/CreateListings.java
+++ app/src/main/java/com/example/jhump/CreateListings.java
@@ -1,6 +1,7 @@
 package com.example.jhump;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -9,6 +10,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.Manifest;
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -22,6 +24,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -32,27 +35,25 @@ import java.util.List;
 
 import static android.app.Activity.RESULT_OK;
 
-public class CreateListings extends Fragment {
+public class CreateListings extends AppCompatActivity {
     private Button newItem;
     private Button cancel;
     private List<Bitmap> pics;
-    private Button gallery;
-    //whereever displaying, use:
+    private ImageButton gallery;
+    //wherever displaying, use:
     // ImageView picture;
     //imageview.setImageBitmap(b);
     //do we want subject?
     private Spinner category;
     private Spinner condition;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View root = inflater.inflate(R.layout.activity_create_listings, container, false);
-        newItem = root.findViewById(R.id.post);
-        cancel = root.findViewById(R.id.cancel);
-        gallery = root.findViewById(R.id.pic);
-        category = root.findViewById(R.id.category);
-        condition = root.findViewById(R.id.condition);
+        newItem = findViewById(R.id.post);
+        cancel = findViewById(R.id.cancel);
+        gallery = findViewById(R.id.pic);
+        category = findViewById(R.id.category);
+        condition = findViewById(R.id.condition);
         final String[] categoryItem = new String[1];
         final String[] conditionItem = new String[1];
         ArrayList<String> cat = new ArrayList<>();
@@ -64,8 +65,8 @@ public class CreateListings extends Fragment {
         con.add("New");
         con.add("Used");
         con.add("Damaged");
-        ArrayAdapter<String> catAdapt = new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, cat);
-        ArrayAdapter<String> conAdapt = new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, con);
+        ArrayAdapter<String> catAdapt = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, cat);
+        ArrayAdapter<String> conAdapt = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, con);
         condition.setAdapter(conAdapt);
         category.setAdapter(catAdapt);
         category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -93,18 +94,13 @@ public class CreateListings extends Fragment {
                 //or throw TOAST?
             }
         });
-        String itemName = ((TextView) root.findViewById(R.id.listing)).getText().toString();
-        String description = ((TextView) root.findViewById(R.id.description)).getText().toString();
-        double price = Double.parseDouble(((TextView) root.findViewById(R.id.price)).getText().toString());
+        String itemName = ((TextView) findViewById(R.id.listing)).getText().toString();
+        String description = ((TextView) findViewById(R.id.description)).getText().toString();
+        double price = Double.parseDouble(((TextView) findViewById(R.id.price)).getText().toString());
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager manager = getActivity().getSupportFragmentManager();
-                FragmentTransaction trans = manager.beginTransaction();
-                trans.remove(new CreateListings());
-                trans.commit();
-                manager.popBackStack();
-                //pop this layout from stack
+
             }
         });
 
@@ -113,9 +109,10 @@ public class CreateListings extends Fragment {
             public void onClick(View v) {
                 //below is to get pics from gallery
                 //was previously createlistings.this before class was made into a frag <---for Chioma
-                if (ActivityCompat.checkSelfPermission(getActivity(),
+                Context context = getApplicationContext();
+                if (ActivityCompat.checkSelfPermission(context,
                         Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+                    ActivityCompat.requestPermissions(CreateListings.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
                     return;
                 }
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -134,7 +131,6 @@ public class CreateListings extends Fragment {
                 //Item addItem = new Item(name, pics, seller, conditionItem, categoryItem, description, price, false);
             }
         });
-        return root;
     }
 
     //should launch camera later on
@@ -149,7 +145,7 @@ public class CreateListings extends Fragment {
                 for (int i = 0; i < clipData.getItemCount(); i++) {
                     Uri imageURI = clipData.getItemAt(i).getUri();
                     try {
-                        InputStream is = getActivity().getContentResolver().openInputStream(imageURI);
+                        InputStream is = this.getContentResolver().openInputStream(imageURI);
                         Bitmap bitmap = BitmapFactory.decodeStream(is);
                         pics.add(bitmap);
                     } catch (FileNotFoundException e) {
@@ -159,7 +155,7 @@ public class CreateListings extends Fragment {
             } else {
                 Uri imageURI = data.getData();
                 try {
-                    InputStream is = getActivity().getContentResolver().openInputStream(imageURI);
+                    InputStream is = this.getContentResolver().openInputStream(imageURI);
                     Bitmap bitmap = BitmapFactory.decodeStream(is);
                     pics.add(bitmap);
                 } catch (FileNotFoundException e) {
