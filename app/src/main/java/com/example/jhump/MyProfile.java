976c7566336391ecfd5d7ee1d1f9771f18eb1200
Alison Lee <00alisonlee@gmail.com>
2020-04-05 13:25:40 -0500

    Made xml layout for MyProfile.

--- app/src/main/java/com/example/jhump/MyProfile.java
+++ app/src/main/java/com/example/jhump/MyProfile.java
@@ -12,17 +12,16 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UserProfile extends Fragment {
+public class MyProfile extends Fragment {
 
-    public UserProfile() {
+    public MyProfile() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        return inflater.inflate(R.layout.fragment_my_profile, container, false);
     }
 }
