5f43e2df13d3dbbdd6dfdbd2d508bcba668fe155
Aaron Rhee <arhee5@jhu.edu>
2020-04-05 16:04:38 -0400

    MainActivity Navigation Bar Implemented + AllListings Basic Fragment Opening

--- app/src/main/java/com/example/jhump/AllListings.java
+++ app/src/main/java/com/example/jhump/AllListings.java
@@ -53,6 +53,7 @@ public class AllListings extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
