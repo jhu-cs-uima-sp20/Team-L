a09cf618922092aad0d638d86f813fad41b1083f
Alison Lee <00alisonlee@gmail.com>
2020-04-05 20:48:34 -0500

    Fixed some weird issue with the layout file for UserProfile not working.

--- app/src/main/java/com/example/jhump/UserProfile.java
+++ app/src/main/java/com/example/jhump/UserProfile.java
@@ -18,7 +18,6 @@ public class UserProfile extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
