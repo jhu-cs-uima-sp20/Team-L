98ae7dce5b49ec5c97cb150c79741f32d90a09d2
Joshua Kim <jkim567@jhu.edu>
2020-04-06 20:04:19 -0500

    trying to fix picture input

--- app/src/main/java/com/example/jhump/CreateListings.java
+++ app/src/main/java/com/example/jhump/CreateListings.java
@@ -1,6 +1,12 @@
 package com.example.jhump;
 
+import android.Manifest;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +19,18 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import static android.app.Activity.RESULT_OK;
 
 public class CreateListings extends Fragment implements View.OnClickListener{
+
     private Button post;
     private Button cancel;
     private List<Bitmap> pics;
@@ -66,28 +78,29 @@ public class CreateListings extends Fragment implements View.OnClickListener{
                 Toast.makeText(parent.getContext(), "Select Condition", Toast.LENGTH_LONG).show();
             }
         });
+
+        gallery = root.findViewById(R.id.pic);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { 
+                if (ActivityCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+                    return;
+                }
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
         post.setOnClickListener(this);
         cancel.setOnClickListener(this);
-//        final String[] categoryItem = new String[1];
-//        final String[] conditionItem = new String[1];
-//        ArrayList<String> cat = new ArrayList<>();
-//        cat.add("Textbook");
-//        cat.add("I-Clicker");
-//        cat.add("Lab Equipment");
-//        ArrayList<String> con = new ArrayList<>();
-//        con.add("Gently Used");
-//        con.add("New");
-//        con.add("Used");
-//        con.add("Damaged");
-
-//        ArrayAdapter<String> conAdapt = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, con);
-//        condition.setAdapter(conAdapt);
-//        category.setAdapter(catAdapt);
         return root;
     }
 
     public boolean checkAllInput(EditText listingName, ImageButton gallery, Spinner condition,
-                                 Spinner category, EditText description, EditText price) {
+                                 Spinner category, EditText price) {
         String listing = listingName.getText().toString();
         //gallery idk how to do?
         //Spinner
@@ -105,7 +118,7 @@ public class CreateListings extends Fragment implements View.OnClickListener{
                 category = view.findViewById(R.id.category);
                 description = view.findViewById(R.id.description);
                 price = view.findViewById(R.id.price);
-                if (!checkAllInput(listingName, gallery, condition, category, description, price)) {
+                if (!checkAllInput(listingName, gallery, condition, category, price)) {
                     CharSequence text = "Please complete all required fields.";
                     int duration = Toast.LENGTH_SHORT;
                     Toast toast = Toast.makeText(getActivity(), text, duration);
@@ -120,92 +133,39 @@ public class CreateListings extends Fragment implements View.OnClickListener{
                 //where should this go to? like maybe
         }
     }
-//
-//        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                categoryItem[0] = parent.getItemAtPosition(position).toString();
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//                categoryItem[0] = "Textbook";
-//                //or throw TOAST?
-//            }
-//        });
-//
-//        condition.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                conditionItem[0] = parent.getItemAtPosition(position).toString();
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//                categoryItem[0] = "Used";
-//                //or throw TOAST?
-//            }
-//        });
-//
-//        cancel.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-
-//        gallery.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //below is to get pics from gallery
-//                //was previously createlistings.this before class was made into a frag <---for Chioma
-//                Context context = getApplicationContext();
-//                if (ActivityCompat.checkSelfPermission(context,
-//                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//                    ActivityCompat.requestPermissions(CreateListings.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-//                    return;
-//                }
-//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-//                intent.setType("image/*");
-//                startActivityForResult(intent, 1);
-//            }
-//        });
-
-//
 
 
-    //should launch camera later on
-    //for launching user gallery to select multiple images
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if(resultCode == RESULT_OK && requestCode == 1) {
-//            pics = new ArrayList<>();
-//            assert data != null;
-//            ClipData clipData = data.getClipData();
-//            if (clipData != null) {
-//                for (int i = 0; i < clipData.getItemCount(); i++) {
-//                    Uri imageURI = clipData.getItemAt(i).getUri();
-//                    try {
-//                        InputStream is = this.getContentResolver().openInputStream(imageURI);
-//                        Bitmap bitmap = BitmapFactory.decodeStream(is);
-//                        pics.add(bitmap);
-//                    } catch (FileNotFoundException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            } else {
-//                Uri imageURI = data.getData();
-//                try {
-//                    assert imageURI != null;
-//                    InputStream is = this.getContentResolver().openInputStream(imageURI);
-//                    Bitmap bitmap = BitmapFactory.decodeStream(is);
-//                    pics.add(bitmap);
-//                } catch (FileNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//    }
+//    should launch camera later on
+//    for launching user gallery to select multiple images
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == 1) {
+            pics = new ArrayList<>();
+            assert data != null;
+            ClipData clipData = data.getClipData();
+            if (clipData != null) {
+                for (int i = 0; i < clipData.getItemCount(); i++) {
+                    Uri imageURI = clipData.getItemAt(i).getUri();
+                    try {
+                        InputStream is = getActivity().getContentResolver().openInputStream(imageURI);
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        pics.add(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                Uri imageURI = data.getData();
+                try {
+                    assert imageURI != null;
+                    InputStream is = getActivity().getContentResolver().openInputStream(imageURI);
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+                    pics.add(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
