f4b763a85179ad920a4fc07f6bc6f27b43c77bd1
Alison Lee <00alisonlee@gmail.com>
2020-04-26 19:34:45 -0500

    Made filters xml

--- app/src/main/java/com/example/jhump/Filters.java
+++ app/src/main/java/com/example/jhump/Filters.java
@@ -18,6 +18,7 @@ public class Filters extends Fragment {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
